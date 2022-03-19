03d58b9e0aaae962382ad1e66789b0ac04c62e19
Omikorin <michal.k2001@gmail.com>
2022-03-19 23:43:13 +0100

    fix parity bit for packets

--- src/coder.py
+++ src/coder.py
@@ -1,7 +1,15 @@
 def create_packet(data, packet_size):
     packets = []
 
-    for i in range(0, len(data), packet_size):
+    wild_bits_count = len(data) % (packet_size - 1)
+
+    # fill bits_count = packet_size - wild_bits_count - 1 (parity_bit)
+    fill_bits_count = packet_size - wild_bits_count - 1
+
+    for i in range(fill_bits_count):
+        data.append(0)
+
+    for i in range(0, len(data), packet_size - 1):
         packet = data[i:i + packet_size - 1]
         packet.append(get_parity_bit(packet))
 
