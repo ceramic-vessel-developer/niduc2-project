d156b12797384fd65475fc1cbe9eca04af3d6fe7
Omikorin <michal.k2001@gmail.com>
2022-03-19 21:56:45 +0100

    add parity bit

--- src/coder.py
+++ src/coder.py
@@ -1,7 +1,23 @@
 def create_packet(data, packet_size):
-    packet_data = []
+    packets = []
 
     for i in range(0, len(data), packet_size):
-        packet_data.append(data[i:i + packet_size])
+        packet = data[i:i + packet_size - 1]
+        packet.append(get_parity_bit(packet))
 
-    return packet_data
+        packets.append(packet)
+
+    return packets
+
+
+def get_parity_bit(data):
+    count = 0
+
+    for bit in data:
+        if (bit == 1):
+            count += 1
+
+    if ((count % 2) == 1):
+        return 1
+    else:
+        return 0
