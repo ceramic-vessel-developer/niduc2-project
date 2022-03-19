3058b0f1ab86dadc67fa0830610f67d818e218bb
Omikorin <michal.k2001@gmail.com>
2022-03-19 21:28:04 +0100

    rename function

--- src/coder.py
+++ src/coder.py
@@ -1,7 +1,7 @@
-def create_package(data, package_size):
-    packaged_data = []
+def create_packet(data, package_size):
+    packet_data = []
 
     for i in range(0, len(data), package_size):
-        packaged_data.append(data[i:i + package_size])
+        packet_data.append(data[i:i + package_size])
 
-    return packaged_data
+    return packet_data
